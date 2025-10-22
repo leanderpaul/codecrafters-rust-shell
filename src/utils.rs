fffa9af64ef7d46d223d1f68fe1bd39beb8b37de
Leander Paul <leanderpaul97@outlook.com>
2025-10-22 21:49:30 +0400

    fix: check if file is executable in type command

--- src/utils.rs
+++ src/utils.rs
@@ -1,4 +1,4 @@
-use std::{fs::DirEntry, path::Path};
+use std::{fs::DirEntry, os::unix::fs::PermissionsExt, path::Path};
 
 pub fn find_command_in_path(cmd: &str) -> Option<DirEntry> {
   let path = std::env::var("PATH").unwrap_or_default();
@@ -12,7 +12,9 @@ pub fn find_command_in_path(cmd: &str) -> Option<DirEntry> {
 
     for entry in path.read_dir().unwrap() {
       let entry = entry.unwrap();
-      if entry.file_name() == cmd {
+      let is_file = entry.path().is_file();
+      let is_executable = entry.path().metadata().unwrap().permissions().mode() & 0o111 != 0;
+      if entry.file_name() == cmd && is_file && is_executable {
         return Some(entry);
       }
     }

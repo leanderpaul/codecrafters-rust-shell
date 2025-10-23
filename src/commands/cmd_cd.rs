26811bbac5480e49f9c38cbf74b18240e627ba48
Leander Paul <leanderpaul97@outlook.com>
2025-10-23 18:57:31 +0400

    feat: cd builtin - Relative paths (GQ9)

--- src/commands/cmd_cd.rs
+++ src/commands/cmd_cd.rs
@@ -1,12 +1,17 @@
-use std::path::Path;
+use std::{fs, path::PathBuf};
 
 use crate::state::STATE;
 
 pub fn execute(args: &[&str]) {
-  let path = Path::new(args.get(0).unwrap());
-  if path.exists() {
-    STATE.write().unwrap().cwd = path.to_path_buf();
-  } else {
-    eprintln!("cd: {}: No such file or directory", path.display());
+  let input = args.get(0).unwrap();
+  let mut path: PathBuf = PathBuf::from(input);
+  if input.starts_with(".") {
+    let cwd = &STATE.read().unwrap().cwd;
+    path = PathBuf::from(cwd).join(path);
+  }
+
+  match fs::canonicalize(path) {
+    Ok(canonical_path) => STATE.write().unwrap().cwd = canonical_path,
+    Err(_) => eprintln!("cd: {}: No such file or directory", input),
   }
 }
